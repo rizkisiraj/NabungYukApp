90beda99ca492d63162b3549c3663d3c6490b349
rizkisiraj <jomirwa@gmail.com>
2024-02-27 19:52:57 +0700

    Initial Commit

---
+++ NabungYukApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NabungYukApp
+//
+//  Created by Rizki Siraj on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NabungYukApp/NabungYukAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NabungYukAppApp.swift
+//  NabungYukApp
+//
+//  Created by Rizki Siraj on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NabungYukAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
